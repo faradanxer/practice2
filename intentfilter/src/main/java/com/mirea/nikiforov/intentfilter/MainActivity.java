b9acd0e77afb1c99741d7e5f1e03260b8b56b20a
Ivan <nim060222@gmail.com>
2021-06-01 21:59:24 +0300

    Initial commit

---
+++ intentfilter/src/main/java/com/mirea/nikiforov/intentfilter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mirea.nikiforov.intentfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Nikiforov Ivan Maximovich");
+        startActivity(Intent.createChooser(shareIntent, "My FIO"));
+
+    }
+}
---
+++ multiactivity/src/main/java/com/mirea/nikiforov/multiactivity/SecondActivity.java
@@ -0,0 +1,41 @@
+package com.mirea.nikiforov.multiactivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.mirea.nikiforov.multiactivity.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        String text = (String) getIntent().getSerializableExtra("key");
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(text);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
